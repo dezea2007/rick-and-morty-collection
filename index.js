43277f3bceb08303e39ff90c52cc92ecb8f79939
Arthur Deze <arthurdeze@gmail.com>
2023-08-20 01:00:36 +0200

    Creating cards, showing a grid of characters, navigate between the different pages

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
 import express from "express";
 import axios from "axios";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+const API_URL = "https://rickandmortyapi.com/api";
+
+let page = 1;
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-	res.render("index.ejs");
+app.get("/", async (req, res) => {
+	try {
+		const characters = await axios.get(
+			`${API_URL}/character/?page=${page}`
+		);
+		const page1 = await axios.get(`${API_URL}/episode`);
+		const page2 = await axios.get(`${API_URL}/episode/?page=2`);
+		const page3 = await axios.get(`${API_URL}/episode/?page=3`);
+		res.render("index.ejs", {
+			characters: characters.data.results,
+			episodes: [
+				...page1.data.results,
+				...page2.data.results,
+				...page3.data.results,
+			],
+			count: characters.data.info.pages,
+			page,
+		});
+	} catch (err) {
+		res.status(404).send(err.message);
+	}
+});
+
+app.post("/pages", (req, res) => {
+	if (req.body.type == "next") {
+		page += 1;
+	} else {
+		page -= 1;
+	}
+	res.redirect("/");
 });
 
 app.listen(port, () => {
